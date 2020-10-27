c347ca2a69bccc32f195bceef1defd765c937219
XingruiYi <yxr374@qq.com>
2020-10-27 17:10:50 +0800

    homework2

---
+++ XingruiYi/hw2/golang/README.md
@@ -0,0 +1,8 @@
+## Homework 2 易兴睿 Golang
+
+1、采用 `os.OpenFile` 以二进制方式读取 `.json` 文件
+
+2、采用官方库 `encoding/json` 中提供的 `json.Unmarshal` 对二进制文件进行解码并以结构体保存
+
+3、采用 官方库`flag`实现读取命令行参数
+
---
+++ XingruiYi/hw2/golang/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"log"
+	"os"
+	"time"
+
+	gosocketio "github.com/graarh/golang-socketio"
+	"github.com/graarh/golang-socketio/transport"
+)
+
+type User struct {
+	ID        string    `json:"id"`
+	RoomID    string    `json:"room_id"`
+	Name      string    `json:"name"`
+	AvatarURL string    `json:"avatarUrl"`
+	Type      string    `json:"type"`
+	Address   string    `json:"address"`
+	LoginTime time.Time `json:"login_time"`
+}
+
+func testRead(filepath string) []byte {
+	fp, err := os.OpenFile(filepath, os.O_RDONLY, 0755)
+	defer fp.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	data := make([]byte, 500)
+	n, err := fp.Read(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return data[:n]
+}
+
+func main() {
+	fileName := "chat.log"
+	logFile, errlog := os.Create(fileName)
+	defer logFile.Close()
+	if errlog != nil {
+		log.Fatalln("open file error !")
+	}
+	chatLog := log.New(logFile, "[Chat]", log.Llongfile)
+
+	config := flag.String("config", "", "Config info")
+	flag.Parse()
+
+	var data []byte
+	data = testRead(*config)
+	user := &User{}
+	json.Unmarshal([]byte(data), user)
+
+	c, err := gosocketio.Dial(
+		gosocketio.GetUrl("124.236.22.20", 3001, false),
+		transport.GetDefaultWebsocketTransport())
+	if err != nil {
+		chatLog.Printf("websocket connection error: %v\n", err)
+		return
+	}
+
+	chatLog.Printf("connect success\n\n")
+
+	_ = c.On("success", func(c *gosocketio.Channel, v interface{}) {
+		chatLog.Println("success:", v)
+	})
+
+	go func() {
+		err = c.Emit("login", user)
+		if err != nil {
+			chatLog.Println("emit error:", err)
+		}
+	}()
+
+	time.Sleep(5 * time.Second)
+	chatLog.Println("finished")
+}
